8e4db0ea30bd38a54dc3de99c5fd94b04a5daaa9
Hafiz Al Asad <hafizalasad712@gmail.com>
2024-02-12 07:23:38 +0600

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,7 +12,7 @@ const Nav = () => {
           <span className="absolute left-0 right-0 bottom-0 w-0 h-0.5 bg-blue-0 opacity-0 group-hover:opacity-100 group-hover:w-full transition-all duration-300"></span>
         </li>
         <li className='relative cursor-pointer group'>
-          <a href="https://drive.google.com/file/d/10qRW9tIEpkLDkPRaCbncLtVUP1QIK5aO/view?usp=drive_link"  target='_blank'><span className='inline-block hover:text-black'>My Resume</span></a>
+          <a href="https://drive.google.com/file/d/1ZEmutcs5gJ03o1A4vOF2h4Tt-VTiIvwv/view"  target='_blank'><span className='inline-block hover:text-black'>My Resume</span></a>
           <span className="absolute left-0 right-0 bottom-0 w-0 h-0.5 bg-blue-0 opacity-0 group-hover:opacity-100 group-hover:w-full transition-all duration-300"></span>
         </li>
         <li className='relative cursor-pointer group'>
